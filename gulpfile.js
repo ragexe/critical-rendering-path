f51d4b4c3b4ddf5acd3b20296b2efb61d345d6f3
ragexe <ragexe@mail.ru>
2017-09-29 01:25:14 +0300

    critical-rendering-path/feature: CNAME del

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+    gulp.src('app')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
+});
 
 gulp.task('mytask', function() {
     console.log('Привет, я таск!');
